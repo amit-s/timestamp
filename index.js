5e204f7c46af95f095578a27310fd65fc703ff32
amit <shettya@gmail.com>
2017-02-06 13:44:14 -0800

    added css styling and middleware for use

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let express = require('express');
 let app = express();
 let port = 3000;
 
+app.use('/css', express.static('public/css'));
+
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/public/index.html');
 });
