da21670c351d5b9cdd698a9476e8c3fbcdcecc48
amit <shettya@gmail.com>
2017-02-06 13:26:26 -0800

    add parseTime function to process string input in URL. Output JSON to webpage

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 'use strict';
-
+let strftime = require('strftime');
 let express = require('express');
 let app = express();
 let port = 3000;
-
-app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/:timestring', function(req,res){
+	let timestring = req.params.timestring;	
+	res.json(parseTime(timestring));
+});
+
 app.listen(port, function(){
 	console.log(`Now listening on port ${port}`);
 });
+
+
+function parseTime(timestring){
+	let pattern = /^\d+$/;
+
+	if(pattern.test(timestring)){
+		if((new Date(+timestring) == "Invalid Date")){
+		return {unix: null, natural: null};
+		}
+		var date = new Date(+timestring);		
+	}else{
+		if((new Date(timestring) == "Invalid Date")){
+		return {unix: null, natural: null};
+		}
+		var date = new Date(timestring);		
+	}
+
+	return {
+		unix: strftime("%s", date),
+		natural: strftime("%B %d, %Y", date)
+	};	
+}
+
